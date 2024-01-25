036b0ed53fef3cdf51f2302324be54effe2f505b
Marco <sanchez.junco@gmail.com>
2024-01-25 09:44:18 -0800

    pokemon search initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center bg-blue-300 h-screen">
+        <div className="my-8"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
