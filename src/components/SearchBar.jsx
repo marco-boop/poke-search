73be17ac2bec766d22406e1834be0ff1fecc3492
Marco <sanchez.junco@gmail.com>
2024-02-06 10:21:23 -0800

    select a pokemon and display data

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import axios from "axios";
 
@@ -11,8 +11,10 @@ import axios from "axios";
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonData, setPokemonData] = useState(null);
+  const heightClass = `h-${pokemonData.height}`;
 
   const fetchPokemon = async () => {
     try {
@@ -21,7 +23,6 @@ const SearchBar = () => {
       );
 
       const firstGenPokemon = response.data.results;
-
       const pokeNames = firstGenPokemon.map((pokemon) => pokemon.name);
 
       if (searchQuery) {
@@ -40,6 +41,29 @@ const SearchBar = () => {
     fetchPokemon();
   }, [searchQuery]);
 
+  const handleSelectPokemon = (pokemonName) => {
+    setSearchQuery(pokemonName);
+    setSelectedPokemon(pokemonName);
+  };
+
+  const fetchSelectedPokemonData = async () => {
+    if (!selectedPokemon) return;
+
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`
+      );
+      // console.log(response.data);
+      setPokemonData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSelectedPokemonData();
+  }, [selectedPokemon]);
+
   return (
     <>
       <h1 className="text-2xl font-bold mb-2 ">Search Pokemon:</h1>
@@ -53,12 +77,13 @@ const SearchBar = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="ml-2 text-lg w-full focus:outline-none"
         />
-        {pokemonList && (
+        {pokemonList && searchQuery && (
           <div className="bg-white text-black w-full z-10 absolute top-14 rounded-md max-h-40 overflow-y-auto">
             {pokemonList.map((pokemon, index) => (
               <p
                 key={index}
                 className="pl-4 hover:bg-yellow-300 cursor-pointer"
+                onClick={() => handleSelectPokemon(pokemon)}
               >
                 {pokemon}
               </p>
@@ -66,6 +91,31 @@ const SearchBar = () => {
           </div>
         )}
       </div>
+      {pokemonData && (
+        <div className="">
+          <h2 className="pt-20 text-xl font-bold mt-4 text-transform: capitalize">
+            {pokemonData.name}
+          </h2>
+          <p className="text-transform: capitalize">
+            {pokemonData.name} is {pokemonData.height} units tall.
+          </p>
+          <img
+            src={pokemonData.sprites.other.home.front_default}
+            alt={pokemonData.name}
+            className={heightClass}
+          />
+
+          {/* <h3>Abilities:</h3> */}
+          {/* <ul>
+            {pokemonData.abilities.map((ability, index) => (
+              <li key={index}>
+                {ability.ability.name} {ability.is_hidden && "(Hidden)"}
+              </li>
+            ))}
+          </ul> */}
+          {/* Display other relevant data here */}
+        </div>
+      )}
     </>
   );
 };
